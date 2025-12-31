0f3c6a1f77306ee8a31cd598d54bff536718965c
Igor Novitskii <drmnnml@gmail.com>
2025-12-31 07:45:16 +0300

    add test login fail

--- go/cmd/auth_test.go
+++ go/cmd/auth_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -20,10 +21,39 @@ func TestLoginSuccess(t *testing.T) {
 
 	res, err := http.Post(ts.URL+"/auth/login", "application/json", bytes.NewReader(data))
 	if err != nil {
-		t.Fatal()
+		t.Fatal(err)
 	}
 	if res.StatusCode != 200 {
 		t.Fatalf("Expected %d got %d", 200, res.StatusCode)
 	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var resData auth.LoginResponse
+	err = json.Unmarshal(body, &resData)
+	if err != nil {
+		t.Fatal()
+	}
+	if resData.Token == "" {
+		t.Fatal("Token not defined")
+	}
+}
 
+func TestLoginFail(t *testing.T) {
+	ts := httptest.NewServer(NewApp())
+	defer ts.Close()
+
+	data, _ := json.Marshal(&auth.LoginRequest{
+		Email:    "a2@a.ru",
+		Password: "1",
+	})
+
+	res, err := http.Post(ts.URL+"/auth/login", "application/json", bytes.NewReader(data))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != 401 {
+		t.Fatalf("Expected %d got %d", 401, res.StatusCode)
+	}
 }
