2746acb1c31051e94073a988bb717e9d7a715cf4
Igor Novitskii <drmnnml@gmail.com>
2026-01-05 23:04:40 +0300

    fix: auth check

--- go/internal/link/handler.go
+++ go/internal/link/handler.go
@@ -31,11 +31,11 @@ func NewLinkHandler(router *http.ServeMux, deps LinkHandlerDeps) {
 		LinkRepository: deps.LinkRepository,
 		EventBus:       deps.EventBus,
 	}
-	router.HandleFunc("POST /link", handler.Create())
+	router.Handle("POST /link", middleware.IsAuthed(handler.Create(), deps.Config))
 	router.Handle("PATCH /link/{id}", middleware.IsAuthed(handler.Update(), deps.Config))
-	router.HandleFunc("DELETE /link/{id}", handler.Delete())
+	router.Handle("DELETE /link/{id}", middleware.IsAuthed(handler.Delete(), deps.Config))
 	router.HandleFunc("GET /link/{hash}", handler.Forward())
-	router.HandleFunc("GET /link", handler.GetAll())
+	router.Handle("GET /link", middleware.IsAuthed(handler.GetAll(), deps.Config))
 }
 
 func (handler *LinkHandler) Create() http.HandlerFunc {
